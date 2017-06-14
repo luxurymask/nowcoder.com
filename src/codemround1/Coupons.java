dc89408ea76ba1f57d3d063341829de38417abc2
liuxl <luxurymask@163.com>
2017-06-14 23:46:49 +0800

    超时的原因：先读数据，再在函数里跑
    像这种大数据量的问题，还是来一条处理一条比较好，不然循环都跑两遍，真的很耗时。

--- src/codemround1/Coupons.java
+++ src/codemround1/Coupons.java
@@ -6,64 +6,56 @@ import java.util.Scanner;
 import java.util.TreeSet;
 
 public class Coupons {
-	public static int check(int n, String[] log) {
-		Map<String, Integer> couponsIn = new HashMap<String, Integer>();
-		Map<String, Integer> couponsOut = new HashMap<String, Integer>();
-		TreeSet<Integer> wildmark = new TreeSet<Integer>();
-
-		for (int i = 0; i < n; i++) {
-			String item = log[i];
-			if (item.equals("?")) {
-				wildmark.add(i);
-				continue;
-			}
-
-			String[] array = item.split(" ");
-			String type = array[0];
-			String id = array[1];
-			if (type.equals("I")) {
-				if (couponsIn.containsKey(id)) {
-					Integer j;
-					Integer inIndex = couponsIn.get(id);
-					if ((j = wildmark.higher((inIndex == null) ? -1 : inIndex)) == null) {
-						return (i + 1);
-					} else {
-						wildmark.remove(j);
-					}
-				}
-				couponsIn.put(id, i);
-				if(couponsOut.containsKey(id)){
-					couponsOut.remove(id);
-				}
-			} else if (type.equals("O")) {
-				if (!couponsIn.containsKey(id)) {
-					Integer j;
-					Integer outIndex = couponsOut.get(id);
-					if ((j = wildmark.higher((outIndex == null) ? -1 : outIndex)) == null) {
-						return (i + 1);
-					} else {
-						wildmark.remove(j);
-					}
-				} else {
-					couponsIn.remove(id);
-				}
-				couponsOut.put(id, i);
-			}
-		}
-
-		return -1;
-	}
-
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		while(true){
+		while (true) {
 			int n = in.nextInt();
 			in.nextLine();
-			String[] log = new String[n];
+			Map<String, Integer> couponsIn = new HashMap<String, Integer>();
+			Map<String, Integer> couponsOut = new HashMap<String, Integer>();
+			TreeSet<Integer> wildmark = new TreeSet<Integer>();
 			for (int i = 0; i < n; i++) {
-				log[i] = in.nextLine();
+				String item = in.nextLine();
+				if (item.equals("?")) {
+					wildmark.add(i);
+					continue;
+				}
+
+				String[] array = item.split(" ");
+				String type = array[0];
+				String id = array[1];
+				if (type.equals("I")) {
+					if (couponsIn.containsKey(id)) {
+						Integer j;
+						Integer inIndex = couponsIn.get(id);
+						if ((j = wildmark.higher((inIndex == null) ? -1 : inIndex)) == null) {
+							System.out.println(i + 1);
+							return;
+						} else {
+							wildmark.remove(j);
+						}
+					}
+					couponsIn.put(id, i);
+					if (couponsOut.containsKey(id)) {
+						couponsOut.remove(id);
+					}
+				} else if (type.equals("O")) {
+					if (!couponsIn.containsKey(id)) {
+						Integer j;
+						Integer outIndex = couponsOut.get(id);
+						if ((j = wildmark.higher((outIndex == null) ? -1 : outIndex)) == null) {
+							System.out.println(i + 1);
+							return;
+						} else {
+							wildmark.remove(j);
+						}
+					} else {
+						couponsIn.remove(id);
+					}
+					couponsOut.put(id, i);
+				}
 			}
-			System.out.println(check(n, log));
+			System.out.println(-1);
 		}
 	}
 }
